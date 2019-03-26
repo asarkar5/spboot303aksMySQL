c21b6f4047164d9731759d6736cfb9d5e2f3134f
asarkar5 <ashoke.k.sarkar@gmail.com>
2019-03-25 21:24:29 -0400

    Initial commit

---
+++ src/main/java/com/springboot_303aks/spboot303aks/Spboot303aksApplication.java
@@ -0,0 +1,13 @@
+package com.springboot_303aks.spboot303aks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spboot303aksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spboot303aksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot_303aks/spboot303aks/Spboot303aksApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springboot_303aks.spboot303aks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spboot303aksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
